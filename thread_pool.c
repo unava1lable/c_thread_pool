7efa7e46d281d94c6f3bdbcfa8f2ad4690568a32
jerry <crjscu@foxmail.com>
2023-03-10 23:09:42 +0800

    add pool_create

---
+++ thread_pool.c
@@ -0,0 +1,60 @@
+#include "thread_pool.h"
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+extern int errno;
+
+static void *thread_work(void * args) {
+    pool_t *pool = (pool_t *)args;
+    pool_work_t *work = NULL;
+
+    for (;;) {
+        pthread_mutex_lock(&pool->queue_lock);
+        while (!pool->pool_head && !pool->shutdown) {
+            pthread_cond_wait(&pool->queue_ready, &pool->queue_lock);
+        }
+
+        if (pool->shutdown) {
+            pthread_mutex_unlock(&pool->queue_lock);
+            pthread_exit(NULL);
+        }
+
+        work = pool->pool_head;
+        pool->pool_head = (pool_work_t *)pool->pool_head->next;
+        pthread_mutex_unlock(&pool->queue_lock);
+        work->work(work->args);
+
+        free(work);
+    }
+    return NULL;
+}
+
+int pool_create(pool_t *pool, size_t max_size) {
+    pool = (pool_t *)malloc(sizeof(pool_t));
+    if (pool == NULL) {
+        return -1;
+    }
+    pool->shutdown = 0;
+    pool->max_thread = max_size;
+    pool->thread_id = (pthread_t *)malloc(sizeof(pthread_t) * pool->max_thread);
+    if (pool->thread_id == NULL) {
+        return -1;
+    }
+    pool->pool_head = NULL;
+    if (pthread_mutex_init(&pool->queue_lock, NULL) != 0) {
+        return -1;
+    }
+    if (pthread_cond_init(&pool->queue_ready, NULL) != 0) {
+        return -1;
+    }
+
+    for (int i = 0; i < pool->max_thread; i++) {
+        if (pthread_create(&(pool->thread_id[i]), NULL, thread_work, (void *)pool) != 0) {
+            return -1;
+        }        
+    }
+    return 0;
+}
