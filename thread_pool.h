c13abe316f95f82b884ce2151d3fc98e17bdd3fb
jerry <crjscu@foxmail.com>
2023-03-10 22:32:31 +0800

    add basic type and function prototype

--- thread_pool.h
+++ thread_pool.h
@@ -2,4 +2,23 @@
 #define _THREAD_POOL_H
 #endif
 
+#include <pthread.h>
 
+typedef struct pool_work {
+    void *(*work)(void *);
+    void *args;
+    struct pool_work *next;
+} pool_work_t;
+
+typedef struct pool {
+    pthread_t *thread_id;
+    pool_work_t *pool_head;
+    pthread_cond_t queue_ready;
+    pthread_mutex_t queue_lock;
+    size_t max_thread;
+    int shutdown;
+} pool_t;
+
+int pool_create(pool_t **pool, size_t max_thread);
+void pool_destroy(pool_t *pool);
+int pool_add_tast(pool_t *pool, void *(*work)(void *), void *arg);
